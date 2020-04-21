1c40497aa78140f9cf4f0c094332d352a19fa902
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-04-20 22:54:33 -0400

    test

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import UnderConstruction from './components/UnderConstruction/UnderConstruction'
 
+// removed UnderConstruction after finishing a new component
 export default (
   <Switch>
     <Route exact path='/' component={Home} />
